37531d33b31c702ba5d2d24a8168e107889f7f3c
Eugene <comradeeugene@gmail.com>
2022-02-07 13:54:52 +0300

    Добавьте файлы проекта.

---
+++ Lesson_8.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace Lesson_8._3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ConsoleKeyInfo cki;
+			HashSet<int> digits = new HashSet<int>();
+
+			do
+			{
+				Clear();
+				WriteLine("Введите число");
+				int temp = int.Parse(ReadLine());
+				if (digits.Contains(temp))
+				{
+					WriteLine("Такой число уже существует");
+				}
+				else
+				{ 
+					digits.Add(temp);
+					WriteLine("Число добавлено");
+				}
+				WriteLine("ESC - выход ENTER - продолжить");
+				cki = ReadKey();
+			} while (cki.Key != ConsoleKey.Escape);
+		}
+	}
+}
